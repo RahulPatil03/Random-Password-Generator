75a43fdb54b6b905de22918002b58fdc0d9bbc72
Rahul Patil <rahulrohidaspatil@gmail.com>
2023-01-11 21:20:41 +0530

    project complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,13 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
 export default function Navbar() {
-    return <AppBar>
-        <Toolbar>
-            <Typography flexGrow={1}>Random Password Generator</Typography>
-            <Button>Login</Button>
-        </Toolbar>
-    </AppBar>
+    return <>
+        <AppBar>
+            <Toolbar>
+                <Typography flexGrow={1}>Random Password Generator</Typography>
+                <Button>Login</Button>
+            </Toolbar>
+        </AppBar>
+        <Toolbar />
+    </>
 }
