ce1977f862a2d1701cbf1dffbd484d6c55a7105f
Rahul Patil <rahulrohidaspatil@gmail.com>
2023-01-11 16:07:21 +0530

    Material UI Added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Random Password Generator</title>
-      </Head>
-      Random Password Generator
-    </>
-  )
+  return <>
+    <Head>
+      <title>Random Password Generator</title>
+    </Head>
+    
+  </>
 }
